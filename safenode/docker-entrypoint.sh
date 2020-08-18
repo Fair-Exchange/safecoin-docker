e232b55c51e09b3f4c103c6d7091aa1991a1ffdd
3DA4C300 <3DA4C300@protonmail.com>
2020-08-18 16:09:51 +0200

    Update url

--- safenode/docker-entrypoint.sh
+++ safenode/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ ! -f $HOME/.safecoin/safecoin.conf ]; then
-    echo "SafeNode hasn't been configured yet. Follow these steps: https://github.com/Fair-Exchange/safenode-docker/#Configure-the-container"
+    echo "SafeNode hasn't been configured yet. Follow these steps: https://github.com/Fair-Exchange/safecoin-docker/tree/master/safenode#configure-the-container"
     trap "exit 1" TERM
     while :; do sleep 1; done
 fi
