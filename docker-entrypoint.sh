17d4a0777004310a48d45374850d101a5e21c714
3DA4C300 <3DA4C300@protonmail.com>
2020-08-23 13:17:42 +0200

    Use Tor to download params, if available

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,22 +7,23 @@ if [ ! -f $HOME/.safecoin/safecoin.conf ]; then
     touch $HOME/.safecoin/safecoin.conf
 fi
 
-echo "Checking fetch-params..."
-fetch-params.sh > /dev/null
-echo "Initialization completed successfully"
-echo
-
 TOR_PROXY=$(getent hosts tor | awk '{ print $1 }')
 if [ ! -z "$TOR_PROXY" ]; then
+    http_proxy="socks5://$TOR_PROXY:9050"
     if [ "$TORNODE" = 1 ]; then
-        EXTRA_ARGS="-proxy=$TOR_PROXY -externalip=$(cat /var/lib/tor/safecoin-node/hostname) -listen -listenonion=0"
+        EXTRA_ARGS="-proxy=$TOR_PROXY:9050 -externalip=$(cat /var/lib/tor/safecoin-node/hostname) -listen -listenonion=0"
     else
-        EXTRA_ARGS="-onion=$TOR_PROXY"
+        EXTRA_ARGS="-onion=$TOR_PROXY:9050"
     fi
 else
     echo "Tor support is disabled because not Tor proxy has been found."
     echo
 fi
+
+echo "Checking fetch-params..."
+fetch-params.sh > /dev/null
+echo "Initialization completed successfully"
+echo
 
 echo "****************************************************"
 echo "Running: safecoind $EXTRA_ARGS $@"
